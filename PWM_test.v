72a14011d4d7a0dbed206609edee7cab0e4d5e3e
Michelle Blum <blummichellec@yahoo.com>
2017-11-14 13:59:30 -0500

    Final Code
    We just changed some names

--- PWM_test.v
+++ PWM_test.v
@@ -1,26 +1,32 @@
-module PWM_test #(parameter n = 10, parameter m = 4)(
+module PWM_test #(parameter n = 8, parameter m = 4)(
 input CLOCK_50, 
 input [1:0] KEY,
 input [17:0] SW,
-output [8:0] LEDG
+output [n:0] LEDG,
+output [35:0] GPIO
 );
 
 wire s, t, h, mil;
-wire [n-1:0] sw_period, sw_duty ;
+wire [n:0] sw_period, sw_duty ;
 
-assign sw_duty[7:0] = SW[7:0];
-assign sw_period[9:0] = SW[17:8];
+//assign the first 8 switches as the brightness and the rest are the frequency
+assign sw_duty[n-1:0] = SW[n-1:0];
+assign sw_period[n:0] = SW[17:n];
 
 
+//setting variables for the core. 
  PWM_core run0( 
  .reset(KEY[1]),
  .clk(mil),
  .switch_in(sw_duty),
  .period(sw_period),
- .compare_result(4'b1111),
- .out(LEDG[8])
+ .byteenable(4'b1111),
+ .out(LEDG[n]),
+ .out_pin(GPIO[m])
 );
 
+
+//extracting data from the clock divider. 
  clock_divider clock0(
 	CLOCK_50,
 	s,		//Seconds
